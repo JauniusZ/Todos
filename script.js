3e5357013586390fd71ddb163c5e33d945908cf3
JauniusZ <zulonas@gmail.com>
2023-07-27 16:12:14 +0700

    add-event-listener

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Hello World!");
+const form = document.getElementById("todoForm");
+const input = document.getElementById("inputField");
+const list = document.querySelector(".todos-list");
+
+form.addEventListener("submit", (event) => {
+  console.log(event.target);
+  event.preventDefault();
+  const title = input.value;
+  input.value = null;
+  if (title === "") return;
+
+  const listItem = document.createElement("li");
+  listItem.textContent = title;
+  list.appendChild(listItem);
+});
