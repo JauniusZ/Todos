303af146671760a67de41caa07af9d27a4916640
JauniusZ <zulonas@gmail.com>
2023-07-29 13:35:52 +0700

    unchecked-todos-printed-on-screen

--- script.js
+++ script.js
@@ -1,15 +1,42 @@
 const form = document.getElementById("todoForm");
 const input = document.getElementById("inputField");
 const list = document.getElementById("list");
+const todos = [];
 
 form.addEventListener("submit", (event) => {
-  console.log(event.target);
   event.preventDefault();
   const title = input.value;
   input.value = null;
   if (title === "") return;
 
-  const listItem = document.createElement("li");
-  listItem.textContent = title;
-  list.appendChild(listItem);
+  todos.push({ title: title, checked: false });
+
+  list.innerHTML = "";
+
+  todos.forEach((todo, index) => {
+    const listItem = document.createElement("li");
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.checked = todo.checked;
+    listItem.appendChild(checkbox);
+
+    const titleText = document.createElement("span");
+    titleText.textContent = todo.title;
+    listItem.appendChild(titleText);
+
+    checkbox.addEventListener("change", () => {
+      todos[index].checked = checkbox.checked;
+      updateUncheckedTodoCount();
+    });
+
+    list.appendChild(listItem);
+  });
+
+  updateUncheckedTodoCount();
 });
+
+function updateUncheckedTodoCount() {
+  const uncheckedTodos = todos.filter((todo) => !todo.checked).length;
+  const todosLeftElement = document.getElementById("todos-left");
+  todosLeftElement.textContent = `Todos left unchecked: ${uncheckedTodos}`;
+}
